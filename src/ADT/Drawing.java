c3454375de4f6374362c16aeb9485a613fa5da8b
Adam Yala <adamyala@mit.edu>
2013-12-02 11:33:52 -0500

    Updating design doc and ADT

--- src/ADT/Drawing.java
+++ src/ADT/Drawing.java
@@ -1,5 +1,22 @@
 package ADT;
 
+import java.awt.Image;
+
+/**
+ * Drawing represents the contents of whiteboard. Classes that implement 
+ * drawing are stroke, path and sketch. 
+ *
+ */
 public interface Drawing {
 
+	/**
+	 *   Deletes all components from the Drawing.
+	 **/
+	public void clear();
+
+	/**
+	 *   Returns associated Java Graphics image associated with this
+	 *   Drawing.  This represents the bridge from the ADT to the GUI.
+	 **/ 
+	public Image updateImage(Image background);
 }
--- src/ADT/Stroke.java
+++ src/ADT/Stroke.java
@@ -1,5 +1,29 @@
 package ADT;
 
-public class Stroke {
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Point;
 
+public class Stroke implements Drawing {
+	
+	private Color color;
+	private double thickness;
+	private Point startPoint;
+	private Point endPoint;
+
+	
+	public Stroke(Point startPoint, Point endPoint, Color c, double pixels){
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
+		this.color = c;
+		this.thickness = pixels;
+	}
+	
+	public void clear()  {
+		
+	}
+	
+	public Image getImage()  {
+		return null;
+	}
 }
