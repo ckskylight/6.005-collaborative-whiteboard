ca71ada68335dbc8f8493ddf7fee1e3ce07499cf
John O'Sullivan <johno@mit.edu>
2013-12-11 15:00:45 -0500

    Made WhiteboardServer remove terminated threads from the readerThreads and writerThreads maps.  Added command line initialization functionality for both WhiteboardServer and WhiteboardWindow.

---
+++ src/GUI/PortCarrier.java
@@ -0,0 +1,15 @@
+package GUI;
+
+public class PortCarrier {
+    int port = 4444;
+    public PortCarrier() {
+    }
+    
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+    public int getPort() {
+        return port;
+    }
+}
